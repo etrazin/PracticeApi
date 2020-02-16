416a8fafa057993f1c904cf5cbe6b328ca41db38
Elchanan Razin <etrazin@gmail.com>
2020-02-16 18:47:01 +0200

    created dbcontext for atm db

---
+++ PracticeApi/PracticeApi.DataAccess/AtmContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PracticeApi.Entities;
+using System;
+
+namespace PracticeApi.DataAccess
+{
+    public class AtmContext:DbContext 
+    {
+        //public AtmContext() { }
+        public AtmContext(DbContextOptions<AtmContext> options)
+            : base(options)
+        { 
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Bank> Banks { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //modelBuilder.Entity<Transaction>()
+            //.HasOne<Bank>()
+            //.WithMany(b => b.Transactions).OnDelete(DeleteBehavior.SetNull);
+
+            //modelBuilder.Entity<Transaction>()
+            //    .HasOne<Customer>()
+            //    .WithMany(c => c.Transactions).OnDelete(DeleteBehavior.SetNull);
+        }
+        
+
+    }
+}
